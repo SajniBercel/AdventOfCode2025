9d9f816042bc28511fa2cbe4c3b4224ed1d8cd89
Sajni Bercel <SajniBercel@gmail.com>
2025-12-03 00:29:45 +0100

    day 2, 2 stars

---
+++ AOC_02/CodeRange.cs
@@ -0,0 +1,91 @@
+﻿using System.Security;
+
+namespace AOC_02
+{
+    internal class CodeRange
+    {
+        ulong _codeA;
+        ulong _codeB;
+
+        public CodeRange(ulong a, ulong b)
+        {
+            _codeA = a; 
+            _codeB = b;
+        }
+
+        #region part1
+        public ulong CountWrongCodes1()
+        {
+            ulong count = 0;
+            for (ulong i = _codeA; i <= _codeB; i++)
+            {
+                if(!Validate1(i))
+                    count += i;
+            }
+
+            return count;
+        }
+
+        private bool Validate1(ulong code)
+        {
+            string Scode = code.ToString();
+
+            if (Scode.Length % 2 == 1)
+                return true;
+
+            string partA = Scode.Substring(0,Scode.Length/2);
+            string partB = Scode.Substring(Scode.Length / 2);
+
+            if(partA.Equals(partB))
+                return false;
+            
+            return true;
+        }
+        #endregion
+
+        #region part2
+        public ulong CountWrongCodes2()
+        {
+            ulong count = 0;
+            for (ulong i = _codeA; i <= _codeB; i++)
+            {
+                for (int j = 1; j <= i.ToString().Length / 2; j++)
+                {
+                    if (!Validate2(i.ToString(), j))
+                    {
+                        count += i;
+                        break;
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        private bool Validate2(string code, int splitLen)
+        {
+            string split = code.Substring(0, splitLen);
+
+            for (int i = splitLen; i < code.Length; i += splitLen)
+            {
+                string temp = "";
+
+                if (i + splitLen <= code.Length)
+                    temp = code.Substring(i, splitLen);
+                else
+                    temp = code.Substring(i);
+
+                if (!split.Equals(temp))
+                    return true;
+            }
+
+            return false;
+        }
+        #endregion
+
+        public override string ToString()
+        {
+            return $"{_codeA} - {_codeB}";
+        }
+    }
+}
---
+++ AOC_02/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace AOC_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<CodeRange> Codes = GetData();
+
+            // part1
+            ulong result1 = 0;
+            for (int i = 0; i < Codes.Count; i++)
+            {
+                result1 += Codes[i].CountWrongCodes1();
+            }
+
+            Console.WriteLine("RESULT1: " + result1);
+
+            // part2
+            ulong result2 = 0;
+            for (int i = 0; i < Codes.Count; i++)
+            {
+                result2 += Codes[i].CountWrongCodes2();
+            }
+
+            Console.WriteLine("RESULT2: " + result2);
+        }
+
+        static List<CodeRange> GetData()
+        {
+            Console.WriteLine("File Path:");
+            string filepath = Console.ReadLine();
+            StreamReader sr = new StreamReader(filepath);
+
+            List<string> lines = new List<string>();
+            while (!sr.EndOfStream)
+            { 
+                lines.Add(sr.ReadLine());
+            }
+            sr.Close();
+
+            List<CodeRange> output = new List<CodeRange>();
+
+            for (int i = 0; i < lines.Count; i++)
+            { 
+                string[] ranges = lines[i].Split(',');
+                for (int j = 0; j < ranges.Length; j++)
+                {
+                    if (ranges[j].Length > 3)
+                    {
+                        string[] parts = ranges[j].Split("-");
+                        output.Add(new CodeRange(ulong.Parse(parts[0]), ulong.Parse(parts[1])));
+                    }
+                }
+            }
+
+            return output;
+        }
+    }
+}
